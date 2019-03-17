10628693d0e4b23a34411f61902c11a28f52ab30
Ching-Yu Chen <chingyuc@yelp.com>
2019-03-17 10:19:38 -0700

    Tmux changes.

--- bashrc/tmux.sh
+++ bashrc/tmux.sh
@@ -1,8 +1,8 @@
-if [ -t 0 ] && [ -z "$TMUX" ] && which tmux >/dev/null 2>&1; then
-  sess=$(tmux list-sessions -F '#{?session_attached,,#{session_name}}' 2>/dev/null |grep -v '^$' | head -1)
+if [ -t 0 ] && [ -z "$TMUX" ] && which agenttmux >/dev/null 2>&1; then
+  sess=$(agenttmux list-sessions -F '#{?session_attached,,#{session_name}}' 2>/dev/null |grep -v '^$' | head -1)
   if [ -n "$sess" ]; then
-    tmux -2 attach-session -t $sess
+    agenttmux -2 attach-session -t $sess
   else
-    tmux -2 new-session
+    agenttmux -2 new-session
   fi
 fi
